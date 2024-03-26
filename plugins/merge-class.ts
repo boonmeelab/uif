a852e2804c8c53df9af5194af48456fd5f14b5b4
Nutdanai (Max) <nutdanai@boonmeelab.com>
2024-03-26 17:49:51 +0700

    adjust cl to mc

--- plugins/merge-class.ts
+++ plugins/merge-class.ts
@@ -2,11 +2,11 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const cl = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+  const mc = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
   return {
     provide: {
-      cl,
+      mc,
     },
   };
 });
